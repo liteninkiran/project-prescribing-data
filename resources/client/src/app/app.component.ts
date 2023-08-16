7628a422e05f3a2ffd41ff437c3e7127a9c44eb7
Kiran Anand <liteninkiran@hotmail.com>
2023-08-16 01:54:55 +0100

    Add Angular Materials

--- resources/client/src/app/app.component.ts
+++ resources/client/src/app/app.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-    title = 'client';
+export class AppComponent implements OnInit, OnDestroy {
 
-    constructor() {
+    private url = 'https://directory.spineservices.nhs.uk/ORD/2-0-0/organisations?PrimaryRoleId=RO177';
+    private data: Observable<any[]> = new Observable();
+    private subscriptions: Subscription[] = [];
+
+    constructor(private http: HttpClient) {
 
     }
 
     public ngOnInit(): void {
 
+    }
+
+    public ngOnDestroy(): void {
+        this.subscriptions.map((sub: Subscription) => sub.unsubscribe())
+    }
+
+    public getDataClick(): void {
+        this.data = this.getData();
+        const sub: Subscription = this.data.subscribe(console.log);
+        this.subscriptions.push(sub);
+    }
+
+    public getData(): Observable<any[]> {
+        return this.http.get<any[]>(`${this.url}`);
     }
 }
--- resources/client/src/app/app.module.ts
+++ resources/client/src/app/app.module.ts
@@ -2,14 +2,69 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTreeModule } from '@angular/material/tree';
 
 @NgModule({
     declarations: [
         AppComponent,
     ],
     imports: [
+        BrowserAnimationsModule,
         BrowserModule,
         AppRoutingModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        MatMenuModule,
+        MatButtonModule,
+        MatIconModule,
+        MatCardModule,
+        MatTabsModule,
+        MatSidenavModule,
+        MatListModule,
+        MatToolbarModule,
+        MatTooltipModule,
+        MatInputModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatProgressSpinnerModule,
+        MatStepperModule,
+        MatDialogModule,
+        MatSelectModule,
+        MatCheckboxModule,
+        MatRadioModule,
+        MatSliderModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatGridListModule,
+        MatTreeModule,
     ],
     providers: [],
     bootstrap: [
