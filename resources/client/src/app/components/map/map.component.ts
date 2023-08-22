a2176f92e06ac3c22b4177bbfc8e78b1486a7cd4
Kiran Anand <liteninkiran@hotmail.com>
2023-08-22 12:42:04 +0100

    Install leaflet.js

--- resources/client/src/app/components/map/map.component.ts
+++ resources/client/src/app/components/map/map.component.ts
@@ -29,6 +29,10 @@ import * as d3 from 'd3';
                         stroke: none;
                     }
 
+                    .map .faded {
+                        opacity: 0.3;
+                    }
+
                 </style>
             </svg>
         </div>
@@ -45,6 +49,8 @@ export class MapComponent implements OnInit, OnDestroy {
     public countries: any;
     public width = 900;
     public height = 600;
+    public scale = 140;
+    private data: any;
 
     constructor(
         element: ElementRef,
@@ -53,28 +59,50 @@ export class MapComponent implements OnInit, OnDestroy {
     }
 
     public ngOnInit(): void {
-        this.projection = d3.geoMercator().scale(140).translate([this.width / 2, this.height / 1.4]);
-        this.path = d3.geoPath(this.projection);
+        this.setProjection();
+        this.setPath();
         this.svg = this.host.select('svg');
         this.g = this.svg.append('g');
-        const url = 'https://cdn.jsdelivr.net/npm/world-atlas@2.0.2/countries-50m.json';
-        d3.json(url).then((data: any) => {
-            this.countries = topojson.feature(data, data.objects.countries);
-
-            this.g
-                .selectAll('path')
-                .data(this.countries.features)
-                .enter()
-                .append('path')
-                .attr('class', 'country')
-                .attr('d', this.path);
-
-        });
-        console.log(this);
+        this.getData();
     }
 
     public ngOnDestroy(): void {
 
     }
 
+    private setProjection(): void {
+        this.projection = d3
+            .geoMercator()
+            .scale(this.scale)
+            .translate([this.width / 2, this.height / 1.4]);
+    }
+
+    private setPath(): void {
+        this.path = d3.geoPath(this.projection);
+    }
+
+    private setData(): void {
+        this.countries = topojson.feature(this.data, this.data.objects.countries);
+        this.g
+            .selectAll('path')
+            .data(this.countries.features)
+            .enter()
+            .append('path')
+            .attr('class', 'country')
+            .attr('d', this.path)
+            .on('mouseenter', (event: MouseEvent, data: any) => {
+                console.log(this.countries);
+                // this.countries.features
+                //     .selectAll('path')
+                //     .classed('faded', true);
+            });
+    }
+
+    private getData(): void {
+        const url = 'https://cdn.jsdelivr.net/npm/world-atlas@2.0.2/countries-50m.json';
+        d3.json(url).then((data: any) => {
+            this.data = data;
+            this.setData();
+        });
+    }
 }
