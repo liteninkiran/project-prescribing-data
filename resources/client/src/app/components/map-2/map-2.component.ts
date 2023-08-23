d6a5fb20f1d2ed4a5f5e1d962f30dca206448233
Kiran Anand <liteninkiran@hotmail.com>
2023-08-23 13:18:39 +0100

    Add leaflet quick start map

--- resources/client/src/app/components/map-2/map-2.component.ts
+++ resources/client/src/app/components/map-2/map-2.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { latLng, tileLayer } from 'leaflet';
+import { icon, Marker } from 'leaflet';
 import * as L from 'leaflet';
 
 @Component({
@@ -8,32 +8,107 @@ import * as L from 'leaflet';
     styleUrls: ['./map-2.component.scss'],
 })
 export class Map2Component implements OnInit {
-
-    private urlTemplate: string = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    private zoom = 13;
+    // Map
+    private urlTemplate: string = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
     private tileLayerOptions: L.TileLayerOptions = {
-        maxZoom: 18,
+        maxZoom: 20,
         attribution: '...',
     };
-    private currentPosition = {
-        lat: 51.505,
-        long: -0.09,
+    private zoom = 13;
+    private centre = [51.505, -0.09];
+    private centreCoords!: L.LatLngExpression;
+
+    // Marker
+    private markerCoords!: L.LatLngExpression;
+    private markerPopupContent: L.Content | ((layer: L.Layer) => L.Content) | L.Popup = '<b>Hello world!</b><br />I am a popup.';
+
+    // Circle
+    private circleCoords!: L.LatLngExpression;
+    private circlePopupContent: L.Content | ((layer: L.Layer) => L.Content) | L.Popup = 'I am a circle.';
+    private circleOptions: L.CircleMarkerOptions = {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500,
     };
 
-    public options = {
-        layers: [ tileLayer(this.urlTemplate, this.tileLayerOptions) ],
-        zoom: this.zoom,
-        center: latLng(this.currentPosition.lat, this.currentPosition.long),
+    // Popup
+    private popupCoords!: L.LatLngExpression;
+    private popupPopupContent: ((source: L.Layer) => L.Content) | L.Content = 'I am a standalone popup.';
+
+    // Polygon
+    private polygonPopupContent: L.Content | ((layer: L.Layer) => L.Content) | L.Popup = 'I am a polygon.';
+    private polygonCoords!: L.LatLngExpression[];
+
+    // User location
+    private getUserLocation = async (): Promise<GeolocationPosition> => {
+        return new Promise((res, rej) => {
+            navigator.geolocation.getCurrentPosition(res, rej);
+        });
     };
+    private userLocation!: GeolocationPosition;
 
     constructor() {
-
     }
 
     public ngOnInit(): void {
-        navigator.geolocation.getCurrentPosition((position) => this.currentPosition = {
-            lat: position.coords.latitude,
-            long: position.coords.longitude,
+        // this.setLocation();
+        this.setCordinates(this.centre[0], this.centre[1]);
+        this.setupMap();
+    }
+
+    private async setLocation(): Promise<void> {
+        if (!this.userLocation) {
+            this.userLocation = await this.getUserLocation();
+            this.setCordinates(this.userLocation.coords.latitude, this.userLocation.coords.longitude);
+            this.setupMap();
+        }
+    }
+
+    private setCordinates(lat: number, long: number) {
+        this.centreCoords = [lat, long];
+        this.markerCoords = [lat - 0.005, long];
+        this.circleCoords = [lat + 0.003, long - 0.02];
+        this.popupCoords = [lat + 0.008, long];
+        this.polygonCoords = [
+            [lat + 0.004, long + 0.010],
+            [lat - 0.002, long + 0.030],
+            [lat + 0.005, long + 0.043],
+        ];
+    }
+
+    private setupMap() {
+        this.fixLeafletBug();
+        const map = L.map('map').setView(this.centreCoords, this.zoom);
+        const tiles = L.tileLayer(this.urlTemplate, this.tileLayerOptions).addTo(map);
+        const marker = L.marker(this.markerCoords).addTo(map).bindPopup(this.markerPopupContent).openPopup();
+        const circle = L.circle(this.circleCoords, this.circleOptions).addTo(map).bindPopup(this.circlePopupContent);
+        const polygon = L.polygon(this.polygonCoords).addTo(map).bindPopup(this.polygonPopupContent);
+        const popup = L.popup().setLatLng(this.popupCoords).setContent(this.popupPopupContent).openOn(map);
+    
+        function onMapClick(e: L.LeafletMouseEvent) {
+            popup.setLatLng(e.latlng)
+                .setContent('You clicked the map at ' + e.latlng.toString())
+                .openOn(map);
+        }
+    
+        map.on('click', onMapClick);
+    }
+
+    private fixLeafletBug() {
+        const iconRetinaUrl = 'assets/marker-icon-2x.png';
+        const iconUrl = 'assets/marker-icon.png';
+        const shadowUrl = 'assets/marker-shadow.png';
+        const iconDefault = icon({
+            iconRetinaUrl,
+            iconUrl,
+            shadowUrl,
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            tooltipAnchor: [16, -28],
+            shadowSize: [41, 41],
         });
+        Marker.prototype.options.icon = iconDefault;
     }
 }
