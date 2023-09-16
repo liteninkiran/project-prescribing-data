f7ed853b1b85adbe43300692ae2e2f27379f23e7
Kiran Anand <liteninkiran@hotmail.com>
2023-09-17 00:13:14 +0100

    Tidy up modules

--- resources/client/src/app/app.module.ts
+++ resources/client/src/app/app.module.ts
@@ -1,47 +1,22 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button'
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatInputModule } from "@angular/material/input";
-import { MatListModule } from "@angular/material/list";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import { MatSelectModule } from "@angular/material/select";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatSortModule } from "@angular/material/sort";
-import { MatTableModule } from "@angular/material/table";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatTreeModule } from '@angular/material/tree';
-import { MatExpansionModule} from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+// Modules
 import { NgxJsonViewerModule } from 'ngx-json-viewer';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { MaterialModule } from './modules/material.module';
+import { PipeModule } from './modules/pipe.module';
+import { AppRoutingModule } from './app-routing.module';
 
+// Components
+import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { OrganisationComponent } from './components/organisation/organisation.component';
 import { OrganisationViewComponent } from './components/organisation-view/organisation-view.component';
-import { ArrayConcatPipe } from './pipes/array-concat.pipe';
-import { ArrayConcatContactPipe } from './pipes/array-concat-contact.pipe';
 import { SamplesComponent } from './components/samples/samples/samples.component';
 import { TableSortComponent } from './components/samples/table-sort/table-sort.component';
 import { TableSortPaginateFilterComponent } from './components/samples/table-sort-paginate-filter/table-sort-paginate-filter.component';
@@ -49,17 +24,13 @@ import { D3MapComponent } from './components/samples/d3-map/d3-map.component';
 import { MapComponent } from './components/map/map.component';
 import { Map2Component } from './components/map-2/map-2.component';
 import { LeafletMapComponent } from './components/samples/leaflet-map/leaflet-map.component';
-import { FormatValuePipe } from './pipes/format-value.pipe';
 import { RoleComponent } from './components/role/role.component';
 import { TableComponent } from './components/shared/table/table.component';
-import { DateAgoPipe } from './pipes/date-ago.pipe';
 import { TableAsyncComponent } from './components/samples/table-async/table-async.component';
 import { FiltersComponent } from './components/role/filters/filters.component';
-import { ArrayFilterPipe } from './pipes/array-filter.pipe';
 
 @NgModule({
     declarations: [
-        // Components
         AppComponent,
         HomeComponent,
         OrganisationComponent,
@@ -73,51 +44,21 @@ import { ArrayFilterPipe } from './pipes/array-filter.pipe';
         LeafletMapComponent,
         RoleComponent,
         TableComponent,
-
-        // Pipes
-        ArrayConcatPipe,
-        ArrayConcatContactPipe,
-        ArrayFilterPipe,
-        FormatValuePipe,
-        DateAgoPipe,
         TableAsyncComponent,
         FiltersComponent,
     ],
     imports: [
+        // Built-in Modules
         BrowserAnimationsModule,
         BrowserModule,
-        AppRoutingModule,
         HttpClientModule,
         FormsModule,
         ReactiveFormsModule,
-        MatMenuModule,
-        MatButtonModule,
-        MatIconModule,
-        MatCardModule,
-        MatTabsModule,
-        MatSidenavModule,
-        MatListModule,
-        MatToolbarModule,
-        MatTooltipModule,
-        MatInputModule,
-        MatTableModule,
-        MatPaginatorModule,
-        MatSortModule,
-        MatProgressSpinnerModule,
-        MatStepperModule,
-        MatDialogModule,
-        MatSelectModule,
-        MatCheckboxModule,
-        MatRadioModule,
-        MatSliderModule,
-        MatDatepickerModule,
-        MatNativeDateModule,
-        MatGridListModule,
-        MatTreeModule,
-        MatExpansionModule,
-        MatFormFieldModule,
-        MatDividerModule,
-        MatSnackBarModule,
+
+        // Custom Modules
+        AppRoutingModule,
+        MaterialModule,
+        PipeModule,
         NgxJsonViewerModule,
         LeafletModule,
     ],
---
+++ resources/client/src/app/modules/material.module.ts
@@ -0,0 +1,64 @@
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatMenuModule } from '@angular/material/menu';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from "@angular/material/select";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from "@angular/material/sort";
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from "@angular/material/table";
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTreeModule } from '@angular/material/tree';
+
+@NgModule({
+    exports: [
+        MatButtonModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatExpansionModule,
+        MatFormFieldModule,
+        MatGridListModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
+        MatMenuModule,
+        MatNativeDateModule,
+        MatPaginatorModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSliderModule,
+        MatSnackBarModule,
+        MatSortModule,
+        MatStepperModule,
+        MatTableModule,
+        MatTabsModule,
+        MatTooltipModule,
+        MatToolbarModule,
+        MatTreeModule,
+    ],
+})
+export class MaterialModule { }
