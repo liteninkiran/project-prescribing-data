6d6d22a1f21e693ea79878f245073fe8a557ef63
Kiran Anand <liteninkiran@hotmail.com>
2023-09-23 23:47:24 +0100

    Tidy up navbar links

--- resources/client/src/app/components/navbar/navbar-links.component.ts
+++ resources/client/src/app/components/navbar/navbar-links.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class NavbarLinksComponent {
 
+    public config: IConfig[] = [
+        {
+            link: '/home',
+            icon: 'home',
+            text: 'Home',
+        },
+        {
+            link: '/organisations-api',
+            icon: 'link',
+            text: 'Organisations API',
+        },
+        {
+            link: '/organisations',
+            icon: 'factory',
+            text: 'Organisations',
+        },
+        {
+            link: '/roles',
+            icon: 'roller_skating',
+            text: 'Roles',
+        },
+        {
+            link: '/samples',
+            icon: 'electrical_services',
+            text: 'Sample Components',
+        },
+    ]
+}
+
+interface IConfig {
+    link: string,
+    icon: string,
+    text: string,
 }
